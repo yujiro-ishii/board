79b83d2d429dadede32ce254bb0a0ae3b72acfe3
Yujiro Ishii <tsunagaru.tsunageru@gmail.com>
2020-04-29 18:52:11 +0900

    Before Symbolic link

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -49,9 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            
+            'screen_name' => ['required', 'string', 'max:255', 'unique:users'],
+            'name'        => ['required', 'string', 'max:255'],
+            'email'       => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password'    => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
@@ -59,11 +61,12 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
         return User::create([
+            'screen_name' => $data['screen_name'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -48,4 +48,58 @@ class User extends Authenticatable
         return $this->belongsToMany(self::class, 'followers', 'following_id', 'followed_id');
     }
 
+    public function getAllUsers(Int $user_id)
+    {
+        return $this->Where('id', '<>', $user_id)->paginate(5);
+    }
+
+     // フォローする
+     public function follow(Int $user_id)
+     {
+         return $this->follows()->attach($user_id);
+     }
+
+     // フォロー解除する
+     public function unfollow(Int $user_id)
+     {
+         return $this->follows()->detach($user_id);
+     }
+
+     // フォローしているか
+     public function isFollowing(Int $user_id)
+     {
+         return (boolean) $this->follows()->where('followed_id', $user_id)->first(['id']);
+     }
+
+     // フォローされているか
+     public function isFollowed(Int $user_id)
+     {
+         return (boolean) $this->followers()->where('following_id', $user_id)->first(['id']);
+     }
+
+
+     public function updateProfile(Array $params)
+    {
+        if (isset($params['profile_image'])) {
+            $file_name = $params['profile_image']->store('public/profile_image/');
+
+            $this::where('id', $this->id)
+                ->update([
+                    'screen_name'   => $params['screen_name'],
+                    'name'          => $params['name'],
+                    'profile_image' => basename($file_name),
+                    'email'         => $params['email'],
+                ]);
+        } else {
+            $this::where('id', $this->id)
+                ->update([
+                    'screen_name'   => $params['screen_name'],
+                    'name'          => $params['name'],
+                    'email'         => $params['email'],
+                ]);
+        }
+
+        return;
+    }
+
 }
