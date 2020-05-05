3ed5310b253ad38142afd5a992962bb8042f3577
Yujiro Ishii <59093539+Yujiro-web@users.noreply.github.com>
2020-05-05 20:51:45 +0900

    Update src/app/Http/Controllers/Auth/RegisterController.php
    Co-authored-by: KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,6 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            
             'screen_name' => ['required', 'string', 'max:255', 'unique:users'],
             'name'        => ['required', 'string', 'max:255'],
             'email'       => ['required', 'string', 'email', 'max:255', 'unique:users'],
