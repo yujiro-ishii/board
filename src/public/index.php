ec5868b05703db1c00b41daad23e00eaba71d487
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2020-05-24 00:17:13 +0900

    Revert "(WIP) improve build process in Docker."
    This reverts commit 3714dfe6876779c08eefc5b6ad3a886c929e3c3e.

--- src/public/index.php
+++ src/public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require '/opt/board/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require '/opt/board/vendor/autoload.php';
 |
 */
 
-$app = require_once '/opt/board/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
